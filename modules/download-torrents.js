06ed4389b4f39b963b6a2ad02aa2961f78e29505
Michal Brašna <michal.brasna@gmail.com>
2015-11-03 23:02:37 +0100

    Create directory for season when required

--- modules/download-torrents.js
+++ modules/download-torrents.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var debug = require('debug')('modules:search-torrents');
+var exec = require('child_process').exec;
+var debug = require('debug')('modules:download-torrents');
 var async = require('async');
 var pad = require('pad');
 
@@ -8,9 +9,24 @@ module.exports = function (synologyOptions, destPath, downloadEpisodes, options,
   var synology = require('../lib/synology')(synologyOptions);
 
   async.mapSeries(downloadEpisodes, function (downloadEpisode, next) {
-    var torrent = downloadEpisode.torrent.torrentLink
+    var torrentLink = downloadEpisode.torrent.torrentLink
     var destination = destPath + downloadEpisode.episode.name + '/Season ' + pad(2, downloadEpisode.episode.season, '0');
-    synology.download(torrent, destination, options.dryRun, next);
+
+    debug('creating remote directory "' + destination + '"');
+    var remoteCommands = [
+      '[ -d \'' + destination + '\' ] || (',
+      'mkdir -p \'' + destination + '\'',
+      ' && chmod 0777 \'' + destination + '\'',
+      ' && chown mbrasna:users \'' + destination + '\'',
+      ')'
+    ]
+    var command = 'ssh nas "' + remoteCommands.join('') + '"';
+    if (options.dryRun) command = 'echo "dry run"';
+    exec(command, function (error, stdout, stderr) {
+      if (error || stderr) return next(error || stderr);
+      synology.download(torrentLink, destination, options.dryRun, next);
+    });
+
   }, done);
 }
 
