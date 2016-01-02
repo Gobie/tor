ab94a748b9def3e2afe71b24239273232ef1a706
Michal Brašna <michal.brasna@gmail.com>
2016-01-02 16:07:32 +0100

    chore(build): add code coverage via istanbul and test for formatters

---
+++ test/formatters.spec.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var assert = require('assert');
+var formatters = require('../lib/formatters');
+
+describe('formatters', function () {
+  describe('episode', function () {
+    it('should handle valid episode', function () {
+      var formattedEpisode = formatters.episode(1, 1);
+      assert.equal(formattedEpisode, 'S01E01');
+    });
+
+    it('should handle malformed episode', function () {
+      var formattedEpisode = formatters.episode(1, undefined);
+      assert.equal(formattedEpisode, 'S01E00');
+    });
+
+    it('should handle malformed season', function () {
+      var formattedEpisode = formatters.episode(null, 5);
+      assert.equal(formattedEpisode, 'S00E05');
+    });
+  });
+
+  describe('filesize', function () {
+    it('should handle valid filesize', function () {
+      var formattedFilesize = formatters.filesize(1e7);
+      assert.equal(formattedFilesize, '9.54 MB');
+    });
+
+    it('should handle malformed filesize', function () {
+      var formattedFilesize = formatters.filesize(null);
+      assert.equal(formattedFilesize, '0 bytes');
+    });
+  });
+});
