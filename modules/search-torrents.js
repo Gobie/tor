8d83be8ecff05f24671d0915faff7911bb1c64d4
Michal Brašna <michal.brasna@gmail.com>
2016-08-09 01:24:50 +0200

    fix(search): query torrent sites with different names when required

--- modules/search-torrents.js
+++ modules/search-torrents.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var async = require('async');
+var _ = require('lodash');
 var formatters = require('../lib/formatters');
 var search = require('../plugins/search/search');
 var filtersFactory = require('../plugins/search/filters');
@@ -10,23 +11,44 @@ module.exports = function (program, episodes, config, done) {
 
   async.mapLimit(episodes, 5, function (episode, next) {
     var query = episode.name + ' ' + formatters.episode(episode.season, episode.episode);
-    program.log.debug('searching for %s', query);
-    search(program, query, function (e, torrents) {
-      if (e || !torrents.length) {
-        program.log.info('episode %s wasn\'t found on torrent sites', query);
+
+    var queries = [query];
+    // TODO extract to config
+    queries.push(query.replace(/Marvel'?s\s*/, ''));
+    queries = _.uniq(queries);
+
+    async.map(queries, function (query, next) {
+      program.log.debug('searching for %s', query);
+      search(program, query, function (e, torrents) {
+        if (e || !torrents.length) {
+          return next(null, []);
+        }
+
+        return next(null, torrents);
+      });
+    }, function (e, torrentsPerQuery) {
+      // No error should ever get here, search should return {}
+      if (e) {
+        program.log.error('searching torrents', e);
+        return next(e);
+      }
+
+      var logQuery = queries.join(' OR ');
+      var torrents = _.flatten(torrentsPerQuery);
+      if (!torrents.length) {
+        program.log.info('episode %s wasn\'t found on torrent sites', logQuery);
         return next();
       }
-      program.log.debug('%s torrents found for %s', torrents.length, query);
 
       var acceptedTorrents = filter(torrents);
-      program.log.debug('%s out of %s torrents remained for %s', acceptedTorrents.length, torrents.length, query);
+      program.log.debug('%s out of %s torrents remained for %s', acceptedTorrents.length, torrents.length, logQuery);
 
       if (!acceptedTorrents.length) {
-        program.log.info('episode %s wasn\'t found because of filters', query);
+        program.log.info('episode %s wasn\'t found because of filters', logQuery);
         return next();
       }
-      program.log.info('episode %s was found', query);
 
+      program.log.info('episode %s was found', logQuery);
       return next(null, {episode: episode, torrent: acceptedTorrents[0]});
     });
   }, function (e, res) {
